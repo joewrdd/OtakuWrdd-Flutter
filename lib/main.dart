78e9e029ad9ccc8101ea2eb1d64795ea28adad69
joe ward <joewrrd@joes-MacBook-Air.local>
2025-03-20 22:37:06 +0200

    Added Push Notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:OtakuWrdd/bin/general_bindings.dart';
+import 'package:OtakuWrdd/data/api/firebase_messaging.dart';
 import 'package:OtakuWrdd/data/repos/authentication/auth.dart';
 import 'package:OtakuWrdd/utils/constants/colors.dart';
 import 'package:OtakuWrdd/utils/constants/images.dart';
@@ -12,12 +13,15 @@ import 'package:get_storage/get_storage.dart';
 import 'firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 Future<void> main() async {
   final WidgetsBinding widgetsBinding =
       WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseMessagingService().init();
   await dotenv.load(fileName: ".env.local");
   Get.put(AuthenticationRepository());
   runApp(const MyApp());
@@ -33,6 +37,7 @@ class MyApp extends StatelessWidget {
       theme: AppTheme.theme,
       initialBinding: GeneralBindings(),
       title: 'Otaku Wrdd',
+      navigatorKey: navigatorKey,
       home: Scaffold(
         body: Stack(
           children: [
